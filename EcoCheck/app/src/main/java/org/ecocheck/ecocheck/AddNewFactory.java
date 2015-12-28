7ccc4b56e0d877305115f80f52bde4876822562e
AharonWeizman <roniwi23@gmail.com>
2015-12-29 00:11:31 +0200

    Create AddNewFactory.java

---
+++ EcoCheck/app/src/main/java/org/ecocheck/ecocheck/AddNewFactory.java
@@ -0,0 +1,13 @@
+package org.ecocheck.ecocheck;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddNewFactory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_factory);
+    }
+}
