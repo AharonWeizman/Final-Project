f083151122e3d84a750c625d9da600b8987cb0c4
AharonWeizman <roniwi23@gmail.com>
2016-06-19 14:59:36 +0300

    Create Material.java

---
+++ EcoCheck/app/src/main/java/org/ecocheck/ecocheck/dto/Material.java
@@ -0,0 +1,109 @@
+package org.ecocheck.ecocheck.dto;
+
+public class Material {
+
+    private String materialIDImp;
+    private String depProcIDImp;
+    private String jobID;
+    private String depProcID;
+    private String TradeName; 
+    private String shemMisharyCode;
+    private String USeMaterial; 
+    private String materialID;
+    private String TinyAmount; 
+    private String msds;
+
+    public String getMaterialIDImp()
+    {
+        return materialIDImp;
+    }
+    public void setMaterialIDImp(String materialIDImp)
+    {
+        this.materialIDImp = materialIDImp;
+    }
+
+    public String getDepProcIDImp()
+    {
+        return depProcIDImp;
+    }
+    public void setDepProcIDImp(String depProcIDImp)
+    {
+        this.depProcIDImp = depProcIDImp;
+    }
+
+    public String getJobID()
+    {
+        return jobID;
+    }
+    public void setJobID(String jobID)
+    {
+        this.jobID = jobID;
+    }
+
+    public String getDepProcID()
+    {
+        return depProcID;
+    }
+    public void setDepProcID(String depProcID)
+    {
+        this.depProcID = depProcID;
+    }
+
+    public String getTradeName()
+    {
+        return TradeName;
+    }
+    public void setTradeName(String TradeName)
+    {
+        this.TradeName = TradeName;
+    }
+
+    public String getShemMisharyCode()
+    {
+        return shemMisharyCode;
+    }
+    public void setShemMisharyCode(String shemMisharyCode)
+    {
+        this.shemMisharyCode = shemMisharyCode;
+    }
+
+    public String getUSeMaterial()
+    {
+        return USeMaterial;
+    }
+    public void setUSeMaterial(String USeMaterial)
+    {
+        this.USeMaterial = USeMaterial;
+    }
+
+    public String getMaterialID()
+    {
+        return materialID;
+    }
+    public void setMaterialID(String materialID)
+    {
+        this.materialID = materialID;
+    }
+
+    public String getTinyAmount()
+    {
+        return TinyAmount;
+    }
+    public void setTinyAmount(String TinyAmount)
+    {
+        this.TinyAmount = TinyAmount;
+    }
+
+    public String getMSDS()
+    {
+        return msds;
+    }
+    public void setMSDS(String msds)
+    {
+        this.msds = msds;
+    }
+
+
+
+
+}
