3e5eec13dedcd8f785b4e6f0d59f5776b5dc1808
AharonWeizman <roniwi23@gmail.com>
2016-06-21 11:09:16 +0300

    Create Constants.java

---
+++ EcoCheck/app/src/main/java/org/ecocheck/ecocheck/utils/Constants.java
@@ -0,0 +1,60 @@
+package org.ecocheck.ecocheck.utils;
+
+/**
+ * Created by Ron on 26/05/2016.
+ */
+
+
+
+public class Constants {
+
+    public static final class DbFields {
+
+
+        public static final String TABLE_NAME_ASSIGNMENT="Assignment";
+        public static final String TABLE_NAME_DEPT= "Department";
+
+        public static final String TABLE_NAME_CHEMICAL_EXPOSURE= "Chemical_Exposure";
+        public static final String TABLE_NAME_PERSONAL_EXPOSURE= "Personal_Exposure";
+        public static final String TABLE_NAME_TYPE_PROCESS_CONTROL= "Type_Process_Control";
+        public static final String TABLE_NAME_MATERIAL_ASSO_TASK= "Material_Asso_Task";
+        public static final String TABLE_NAME_FACTOR_MONITORING_TASK= "Factor_Monitoring_Task";
+
+        public static final String COLUMN_ASSIGNMENT_NAME ="assignment";
+        public static final String COLUMN_NO_OF_EMPLOYEE ="numOfEmployee";
+
+        public static final String COLUMN_DEPARTMENT_NAME = "department";
+        public static final String COLUMN_REPORT_NUMBER = "ReportNumber";
+        public static final String COLUMN_DB_ID = "_id";
+
+        public static final String COLUMN_CHEMICAL_EXPOSURE_NAME = "chemical_inhalant";
+        public static final String COLUMN_CHEMICAL_DESC_INHALENT_EXPOSURE = "chemical_desc_inhalant_exposure";
+
+        public static final String COLUMN_PERSONAL_PROTECTIVE_EQUIP_TYPE="personal_protective_equip_type";
+        public static final String COLUMN_PERSONAL_CONTROL_DESC ="personal_control_desc";
+
+        public static final String COLUMN_TYPE_PROCESS_CONTROL="type_of_process_control";
+        public static final String COLUMN_DESC_PROCESS_CONTROL="desc_process_control";
+
+        public static final String COLUMN_MATERIAL_ASSOCIATED_TASK="material_associated_task";
+        public static final String COLUMN_MATERIAL_TRADE_NAME ="material_trade_name";
+        public static final String COLUMN_USING_MATERIAL="using_material";
+        public static final String COLUMN_LINKED ="linked";
+        public static final String COLUMN_TINY_AMOUNT ="tiny_amount";
+        public static final String COLUMN_MSDS="msds";
+
+        public static final String COLUMN_QUICK_CHOICE="quick_choice";
+        public static final String COLUMN_FACTOR="factor";
+        public static final String COLUMN_POLICY_LEVEL="policy_level";
+        public static final String COLUMN_REGULARITY_TYPE="regularity_type";
+        public static final String COLUMN_UNIT="unit";
+        public static final String COLUMN_NO_OF_SAMPLE="no_of_sample";
+        public static final String COLUMN_INGREDIENTS="ingredients";
+        public static final String COLUMN_SEARCH_ADD="search_add";
+        public static final String COLUMN_MATERIAL_COMPONENT_PRESENT="material_component_present";
+
+
+    }
+
+}
+
